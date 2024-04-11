433ff395e6cb1bde316b01505ccca11f4bb92b39
Crystal <crystalleefetterman@gmail.com>
2024-04-11 11:46:54 -0700

    removed global fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
-  app: {
-    head: {
-      link: [
-        // <link rel="preload" href="Mona-Sans.woff2" as="font" type="font/woff2" crossorigin>
-        { rel: "preload", href: "/Mona%20Sans/Mona-Sans.woff2", as: "font", type: "font/woff2", crossorigin: "anonymous" }
-      ]
-    }
+  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    public: {
+      builderioKey: process.env.BUILDERIO_KEY,
+    },
   },
-    runtimeConfig: {
-  public: {
-    builderioKey: process.env.BUILDERIO_KEY
-  },
-},
-})
+});
